f1f6d0750f179f8c5a43ae5ad861634d3917d260
Chefsito21 <107816641+Chefsito21@users.noreply.github.com>
2026-05-13 12:34:28 -0600

    Mejora de navegación

--- src/lib/pocketbaseClient.js
+++ src/lib/pocketbaseClient.js
@@ -1,5 +1,10 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+// Detectamos la IP actual de la URL del navegador
+const hostname = window.location.hostname;
+
+// Si estás en localhost, usará 127.0.0.1. 
+// Si estás desde el móvil, usará la IP de tu Mac automáticamente.
+const pb = new PocketBase(`http://${hostname}:8090`);
 
 export default pb;
